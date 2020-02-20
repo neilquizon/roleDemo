e15eb9afe32b0935b969914fda50da99ea225a7d
neilquizon <neil.quizon@gmail.com>
2020-02-20 12:57:15 -0800

    Add project files.

---
+++ roleDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using roleDemo.ViewModels;
+
+namespace roleDemo.Data {
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define entity collections.
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<CustomUser> CustomUsers { get; set; }
+
+        // Use this method to define composite primary keys and foreign keys.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // required.
+            base.OnModelCreating(modelBuilder);
+
+            //-------------------------------------------------------
+            // *** Define composite primary keys here. ***
+
+            // This is sample syntax for defining a primary key.
+            // modelBuilder.Entity<ProductSupplier>()
+            //             .HasKey(ps => new { ps.ProductID, ps.SupplierID });
+
+            //-------------------------------------------------------
+            // *** Define composite foreign keys here. ***
+            modelBuilder.Entity<Invoice>()
+                .HasOne(c => c.CustomUser)
+                .WithMany(c => c.Invoices)
+                .HasForeignKey(fk => new { fk.UserName })
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
+        }
+    }
+}
---
+++ roleDemo/Data/CustomUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roleDemo.Data
+{
+    public class CustomUser
+    {
+        [Key]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        // Child
+        public virtual ICollection<Invoice> Invoices { get; set; }
+    }   
+}
